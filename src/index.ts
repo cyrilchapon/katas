14b63d35756dc3ba9b321cac2b7750b7b925c7db
Cyril CHAPON <cyril@permettezmoideconstruire.fr>
2021-09-02 14:00:53 +0200

    First implementation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,96 @@
-console.log('hey ho')
+// const books = [
+//   0, 0,
+//   1,
+//   2, 2,
+//   3,
+//   4, 4, 4
+// ]
+
+import { bookPrice, discounts } from './prices'
+import { sum, replaceAtIndex } from './util'
+
+const decrementAtIndex = replaceAtIndex<number>(
+  old => old - 1
+)
+
+const getBestPrice = (cartBooks: number[]): number => {
+  const bundles = buildBundles(cartBooks)
+  const bundlePrices = bundles.map(getPriceForBundle)
+
+  const cartPrice = sum(...bundlePrices)
+
+  return cartPrice
+}
+
+const buildBundles = (cartBooks: number[]): number[][] => {
+  if (cartBooks.length === 0) {
+    return []
+  }
+
+  // Get quantities at indexes
+  // Like [2, 1, 0, 4, 2]
+  let booksQties = getBooksQuantities(cartBooks)
+
+  // Get indexes sorted desc by quantities
+  // Like [3, 4, 0, 1, 2]
+  const booksIndexes = booksQties.map((v, i) => i)
+  const booksIndexesByQtyDesc = [...booksIndexes]
+    .sort((iBook1, iBook2) => booksQties[iBook1] - booksQties[iBook2])
+    .reverse()
+
+  // Build bundles starting, following highest quantities order
+  // Like [ [3, 4, 0, 1], [3, 4, 0], [3], [3] ]
+  let bundles: number[][] = []
+  while(booksQties.some(qty => qty > 0)) {
+    // Build each bundle by accumulating items
+    let [
+      bundle,
+      newBooksQties
+    ] = booksIndexesByQtyDesc.reduce<[number[], number[]]>(
+      ([bundle, prevBooksQties], bookIndex) => {
+        if (prevBooksQties[bookIndex] === 0) {
+          return [bundle, prevBooksQties]
+        }
+
+        return [
+          [...bundle, bookIndex],
+          decrementAtIndex(prevBooksQties, bookIndex)
+        ]
+      },
+      [[], booksQties]
+    )
+
+    bundle = [...bundle].sort()
+    bundles.push(bundle)
+
+    // At each iteration, update quantities with consumption
+    booksQties = newBooksQties
+  }
+
+  return bundles
+}
+
+const getBooksQuantities = (cartBooks: number[]): number[] => {
+  const maxBookIndex = Math.max(...cartBooks)
+
+  const booksQties = cartBooks.reduce<number[]>((acc, bookId) => {
+    acc[bookId]++
+    return acc
+  }, (new Array(maxBookIndex + 1)).fill(0))
+
+  return booksQties
+}
+
+const getPriceForBundle = (bundleBooks: number[]): number => {
+  if (bundleBooks.length === 0) {
+    return 0
+  }
+
+  const discount = discounts[bundleBooks.length - 1]
+
+  return (bundleBooks.length * bookPrice) * (1 - discount)
+}
+
+export {
+  getBestPrice
+}
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,36 @@
 import { expect } from 'earljs'
 
-describe('initial test', () => {
-  expect(1).toEqual(1)
+import { getBestPrice } from '../src/index'
+
+import {
+  TestCase,
+  basics,
+  simpleDiscounts,
+  severalDiscounts,
+  edgeCases
+} from './test-cases'
+
+const testTheCase = (testCase: TestCase) => {
+  it('returns right amount', () => {
+    const actual = getBestPrice(testCase.books)
+    const expected = testCase.expected
+
+    expect(actual).toEqual(expected)
+  })
+}
+
+describe('basics', () => {
+  basics.forEach(testTheCase)
+})
+
+describe('simpleDiscounts', () => {
+  simpleDiscounts.forEach(testTheCase)
+})
+
+describe('severalDiscounts', () => {
+  severalDiscounts.forEach(testTheCase)
+})
+
+describe('edgeCases', () => {
+  edgeCases.forEach(testTheCase)
 })
